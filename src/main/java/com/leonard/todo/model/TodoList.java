83649a7d3c8c6602b08d616a7c4e153a9f42de15
leonard.wolf <lionmandota@web.de>
2020-03-02 22:07:18 +0100

    ( ^ ᗜ ^ )

--- src/main/java/com/leonard/todo/model/TodoList.java
+++ src/main/java/com/leonard/todo/model/TodoList.java
@@ -1,10 +1,41 @@
 package com.leonard.todo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "todo_list")
 public class TodoList implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String name;
+
+    public TodoList() {
+    }
+
+    public TodoList(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
