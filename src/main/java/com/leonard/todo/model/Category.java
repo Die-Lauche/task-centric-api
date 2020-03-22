9588b3ed4d65a22b202815a62338de5277090edd
leonard.wolf <lionmandota@web.de>
2020-03-22 16:04:01 +0100

    added Category

--- src/main/java/com/leonard/todo/model/Category.java
+++ src/main/java/com/leonard/todo/model/Category.java
@@ -1,9 +1,34 @@
 package com.leonard.todo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "categories")
-public class Category {
+public class Category  implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    public Category() {
+
+    }
+
+    public Category(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
